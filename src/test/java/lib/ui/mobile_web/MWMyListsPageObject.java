717991edc361ee945c0561aaff6c51df8420b4c2
evgenia_barabash <barabash.evgeniya@gmail.com>
2023-03-05 19:12:50 +0300

    lesson 9 task 1

--- src/test/java/lib/ui/mobile_web/MWMyListsPageObject.java
+++ src/test/java/lib/ui/mobile_web/MWMyListsPageObject.java
@@ -7,7 +7,7 @@ public class MWMyListsPageObject extends MyListsPageObject {
 
     static {
         ARTICLE_BY_TITLE_TPL = "xpath://ul[contains(@class,'watchlist')]//h3[contains(text(),'%s')]";
-        REMOVE_FROM_SAVED_BUTTON = "xpath://ul[contains(@class,'watchlist')]//h3[contains(text(),'%s')]/ancestor::a[contains(@class,'title')]/following-sibling::a";
+        REMOVE_FROM_SAVED_BUTTON = "xpath://ul[contains(@class,'watchlist')]//h3[contains(text(),'%s')]/ancestor::a[contains(@class,'title')]/following-sibling::a[contains(@class,'watched')]";
     }
 
     public MWMyListsPageObject(RemoteWebDriver driver) {
